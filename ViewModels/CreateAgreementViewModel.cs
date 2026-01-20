2ac62d3d506b504941eee50cd9c57a1a6da4896e
Yevhen <zhekasoliar26@gmail.com>
2026-01-20 21:09:30 +0100

    Add AgreementViewModel

---
+++ ViewModels/CreateAgreementViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimeExchangePlatform.ViewModels
+{
+    public class CreateAgreementViewModel
+    {
+        
+        public int OfferId { get; set; }
+        [Required(ErrorMessage = "Hours are required.")]
+        [Range(1, 10, ErrorMessage = "Hours must be between 1 and 10.")]
+        public int Hours { get; set; }
+    }
+}
