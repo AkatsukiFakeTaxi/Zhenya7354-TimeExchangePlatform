25f892abd145d6704aa904067ca9042dee4db6ec
iaath1 <mykhailostozhkov@gmail.com>
2026-01-27 06:56:03 +0100

    Add project files.

---
+++ Controllers/AgreementController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TimeExchangePlatform.Models;
+using TimeExchangePlatform.Services;
+using TimeExchangePlatform.ViewModels;
+
+namespace TimeExchangePlatform.Controllers
+{
+    [Authorize]
+    public class AgreementController : Controller
+    {
+        private readonly IAgreementService _agreementService;
+        private readonly IOfferService _offerService;
+        private readonly UserManager<User> _userManager;
+        public AgreementController(IAgreementService agreementService, UserManager<User> userManager,IOfferService offerService)
+        {
+            _agreementService = agreementService;
+            _userManager = userManager;
+            _offerService = offerService;
+        }
+        [HttpGet]
+        public IActionResult CreateAgreement(int offerId)
+        {
+            var offer = new CreateAgreementViewModel
+            {
+                OfferId = offerId
+            };
+            return View(offer);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateAgreement(CreateAgreementViewModel viewModel)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+            var receiverUserId = _userManager.GetUserId(User) ?? "0";
+            var offer = await _offerService.GetOfferByIdAsync(viewModel.OfferId);
+            try
+            {
+                await _agreementService.CreateAgreementAsync(viewModel.OfferId, viewModel.Hours, receiverUserId);
+            }
+            catch(KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            // Make separate method to deactivate offer
+            offer.IsActive = false;
+            await _offerService.UpdateOffer(offer);
+
+            return RedirectToAction("Index", "Offer");
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetUserAgreements()
+        {
+            var userId = _userManager.GetUserId(User) ?? "0";
+            var agreements = await _agreementService.GetUserAgreementsAsync(userId);
+            return View(agreements);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteAgreement(int agreementId)
+        {
+            var result = await _agreementService.DeleteAgreementAsync(agreementId);
+            if(result == -1)
+            {
+                return NotFound($"Agreement with {agreementId} does not exist");
+            }
+            return RedirectToAction("GetUserAgreements");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CancelAgreement(int agreementId)
+        {
+            try
+            {
+                await _agreementService.ChangeStatus(ExchangeStatus.Cancelled, agreementId);
+            }
+            catch(KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+                return RedirectToAction("GetUserAgreements");
+            
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TimeExchangePlatform.Models
+{
+    public class User : IdentityUser
+    {
+        public int Id { get; set; }
+        public string? FullName { get; set; }
+        public string? Bio { get; set; }
+        public string? City { get; set; }
+        public string? ProfilePictureUrl { get; set; }
+        public string? PhoneNumber { get; set; }
+        public int TimePoints { get; set; } = 0;
+        public bool IsPremiumMember { get; set; } = false;
+        public DateTime RegisteredAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
